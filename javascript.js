7f597ffc78bd6640e0a685f2f9be89be32d6c383
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2023-10-21 04:14:58 -0700

    Feat: Add setInterval function and buttons
    Start and stop buttons activate and deactivate interval.
    
    Feat: Add block stacking
    
    Active block tests if stacked block located underneath.
    Active block freezes on last row or if block located underneath.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
-let selectedParent = 'r10';
+let selectedParent = 'r1';
 let selectedChild = 's5';
 console.log(selectedParent);
 console.log(selectedChild);
 
+let nextParent = '';
+let nextChild = '';
+
 function colorSquare() {
-  const selectedSquare = document.querySelector('#r10 #s5');
+  const selectedSquare = document.querySelector('#r1 #s5');
   selectedSquare.style.backgroundColor = ('black');
-  selectedSquare.setAttribute('id', 'active');
+  selectedSquare.setAttribute('class', 'active square');
 }
 
 colorSquare();
@@ -31,67 +34,136 @@ function moveSquare() {
 moveSquare();
 
 function goUp() {
-  const selectedSquare = document.getElementById('active');
-  selectedSquare.style.backgroundColor = ('rgb(196, 242, 222)');
-  selectedSquare.setAttribute('id', `${selectedChild}`);
+  const selectedSquare = document.querySelector('.active');
 
-  const parent = selectedSquare.parentNode;
-  const parent2 = parent.previousElementSibling;
-  selectedParent = parent2.getAttribute('id');
+  if (selectedParent === 'r1') {   //Keeps active square from leaving top grid border
+    return;
+  } else {
+    selectedSquare.style.backgroundColor = ('rgb(196, 242, 222)');
+    selectedSquare.setAttribute('class', 'square');
 
-  const newSquare = document.querySelector(`#${selectedParent} #${selectedChild}`);
-  selectedChild = newSquare.getAttribute('id');
-  newSquare.style.backgroundColor = ('black');
-  newSquare.setAttribute('id', 'active');
+    const parent = selectedSquare.parentNode;
+    const parent2 = parent.previousElementSibling;
+    selectedParent = parent2.getAttribute('id');
 
-  console.log(selectedParent);
-  console.log(selectedChild);
+    const newSquare = document.querySelector(`#${selectedParent} #${selectedChild}`);
+    selectedChild = newSquare.getAttribute('id');
+    newSquare.style.backgroundColor = ('black');
+    newSquare.setAttribute('class', 'active square');
+
+    console.log(selectedParent);
+    console.log(selectedChild);
+  }
 }
 
 function goDown() {
-  const selectedSquare = document.getElementById('active');
-  selectedSquare.style.backgroundColor = ('rgb(196, 242, 222)');
-  selectedSquare.setAttribute('id', `${selectedChild}`);
+  const selectedSquare = document.querySelector('.active');
+  console.log("executed goDown");
 
-  const parent = selectedSquare.parentNode;
-  const parent2 = parent.nextElementSibling;
-  selectedParent = parent2.getAttribute('id');
+  if (selectedParent === 'r20') {   //Keeps active square from leaving grid and freezes it
+    selectedSquare.setAttribute('class', 'stacked square');
+    colorSquare();
+    selectedParent = 'r1';
+    selectedChild = 's5';
 
-  const newSquare = document.querySelector(`#${selectedParent} #${selectedChild}`);
-  selectedChild = newSquare.getAttribute('id');
-  newSquare.style.backgroundColor = ('black');
-  newSquare.setAttribute('id', 'active');
+  } else if (selectedParent !== 'r20') {   //If runs on r20 - ERROR
+    const parentTest = selectedSquare.parentNode;
+    const parent2Test = parentTest.nextElementSibling;
+    nextParent = parent2Test.getAttribute('id');
+    const nextSquare = document.querySelector(`#${nextParent} #${selectedChild}`);
+    const test = nextSquare.getAttribute('class');
+    console.log(test);                     //gets the class of the next square
 
-  console.log(selectedParent);
-  console.log(selectedChild);
+    if (test === 'stacked square') {       //tests if the next square is already stacked and freezes
+      selectedSquare.setAttribute('class', 'stacked square');
+      colorSquare();
+      selectedParent = 'r1';
+      selectedChild = 's5';
+
+    } else {
+      selectedSquare.style.backgroundColor = ('rgb(196, 242, 222)');
+      selectedSquare.setAttribute('class', 'square');
+
+      const parent = selectedSquare.parentNode;
+      const parent2 = parent.nextElementSibling;
+      selectedParent = parent2.getAttribute('id');
+
+      const newSquare = document.querySelector(`#${selectedParent} #${selectedChild}`);
+      selectedChild = newSquare.getAttribute('id');
+      newSquare.style.backgroundColor = ('black');
+      newSquare.setAttribute('class', 'active square');
+
+      console.log(selectedParent);
+      console.log(selectedChild);
+    }
+  } else {}
 }
 
 function goRight() {
-  console.log("fireright");
-  const selectedSquare = document.getElementById('active');
-  selectedSquare.style.backgroundColor = ('rgb(196, 242, 222)');
-  selectedSquare.setAttribute('id', `${selectedChild}`);
+  const selectedSquare = document.querySelector('.active');
 
-  const newSquare = selectedSquare.nextElementSibling;
-  selectedChild = newSquare.getAttribute('id');
-  newSquare.style.backgroundColor = ('black');
-  newSquare.setAttribute('id', 'active');
+  if (selectedChild === 's10') {   //Keeps active square from leaving right grid border
+    return;
+  } else {
+    selectedSquare.style.backgroundColor = ('rgb(196, 242, 222)');
+    selectedSquare.setAttribute('class', 'square');
 
-  console.log(selectedParent);
-  console.log(selectedChild);
+    const newSquare = selectedSquare.nextElementSibling;
+    selectedChild = newSquare.getAttribute('id');
+    newSquare.style.backgroundColor = ('black');
+    newSquare.setAttribute('class', 'active square');
+
+    console.log(selectedParent);
+    console.log(selectedChild);
+  }  
 }
+
 function goLeft() {
-  console.log("fireleft");
-  const selectedSquare = document.getElementById('active');
-  selectedSquare.style.backgroundColor = ('rgb(196, 242, 222)');
-  selectedSquare.setAttribute('id', `${selectedChild}`);
+  const selectedSquare = document.querySelector('.active');
 
-  const newSquare = selectedSquare.previousElementSibling;
-  selectedChild = newSquare.getAttribute('id');
-  newSquare.style.backgroundColor = ('black');
-  newSquare.setAttribute('id', 'active');
+  if (selectedChild === 's1') {   //Keeps active square from leaving left grid border
+    return;
+  } else {
+    selectedSquare.style.backgroundColor = ('rgb(196, 242, 222)');
+    selectedSquare.setAttribute('class', 'square');
 
-  console.log(selectedParent);
-  console.log(selectedChild);
+    const newSquare = selectedSquare.previousElementSibling;
+    selectedChild = newSquare.getAttribute('id');
+    newSquare.style.backgroundColor = ('black');
+    newSquare.setAttribute('class', 'active square');
+
+    console.log(selectedParent);
+    console.log(selectedChild);
+  }
 }
 
+function buttons() {
+  const startButton = document.getElementById('start');
+  const stopButton = document.getElementById('stop');
+
+  startButton.addEventListener('click', drop);
+  stopButton.addEventListener('click', stopDrop);
+}
+buttons()
+
+
+
+let dropInterval = '';
+
+function drop() {
+  dropInterval = setInterval(goDown, 1000);
+}
+
+function stopDrop() {
+  clearInterval(dropInterval);
+}
+
+
+/* function stay() {
+  const selectedSquare = document.getElementById('active');
+
+  if (selectedParent === 'r20') {   //If at the bottom
+    stopDrop();
+  } else {}
+}
+stay(); */
